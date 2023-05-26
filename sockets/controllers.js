cdea3a999d9e01dbb217ac44b483295c3913c32c
Usuario ILINF <admin-ilinf@uao.edu.co>
2023-05-25 20:18:07 -0500

    Firts push

---
+++ sockets/controllers.js
@@ -0,0 +1,42 @@
+let chats = [];
+
+const socketController = (socket, io) => {
+    socket.on("welcome", user => {
+        // Crea la sala del restaurante o añade el socket
+        chats.push(socket);
+
+        socket.name = user.name;
+
+        // Avisar del cambio de la Lista de Usuarios
+        const lista = chats.map(client => client.name);
+        io.emit("lista-usuarios", lista);
+
+        // Broadcast sobre ingreso de usuario
+        socket.broadcast.emit("nuevo-usuario", "El usuario " + socket.name + " ha ingresado a la sala");
+    });
+
+    /*socket.on("message", msg => {
+        // Enviar Mensaje a todos los sockets de la sala
+        let room = chats[socket.room];
+        room.forEach(client => {
+            client.emit("message", msg);
+        });
+    });*/
+
+    const leave = () => {
+        let i = 0;
+        // Borra el Usuario del Chat
+        chats = chats.filter((cliente) => cliente != socket);
+
+        // Avisar del cambio de la Lista de Usuarios
+        const lista = chats.map(x => x.name);
+        io.emit("lista-usuarios", lista);
+
+        // Broadcast sobre ingreso de usuario
+        socket.broadcast.emit("salida-usuario", "El usuario " + socket.name + " se ha desconectado");
+    }
+
+    socket.on("disconnect", leave);
+};
+
+module.exports = { socketController };
